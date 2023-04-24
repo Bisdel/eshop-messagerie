d3bcd192d3caa9274161b7119c1e0d158fe3cb5c
Bisdel <bricotte.leo@gmail.com>
2023-04-24 14:40:39 +0200

    created class attributes

--- eshop-messagerie/src/main/java/fr/formation/model/Message.java
+++ eshop-messagerie/src/main/java/fr/formation/model/Message.java
@@ -1,5 +1,37 @@
 package fr.formation.model;
 
+import java.time.LocalDateTime;
+
 public class Message {
-    
+
+    private int id;
+
+    private LocalDateTime date;
+
+    private String contenu;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
 }
