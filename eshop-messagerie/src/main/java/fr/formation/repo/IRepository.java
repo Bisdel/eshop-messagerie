b4d94c6d3d690d78553ab98095bfdc84879665b0
Massi1996 <benchabamassipssa@gmail.com>
2023-04-24 15:04:59 +0200

    Irepository

--- eshop-messagerie/src/main/java/fr/formation/repo/IRepository.java
+++ eshop-messagerie/src/main/java/fr/formation/repo/IRepository.java
@@ -1,5 +1,18 @@
 package fr.formation.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IRepository<T> {
-    
+public List<T> findAll();
+	
+	public Optional<T> findById( int id);
+	
+	
+	public T save(T entity);
+	
+	
+	public void deleteById( int id);
+	
+	
 }
